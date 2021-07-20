e0a8b6f794a67a0cc1092adfd0a8512b9e9ae93c
aaallliiis <alisolatni59@icloud.com>
2021-07-20 16:23:47 +0430

    complete:register

--- src/views/User/Register/index.jsx
+++ src/views/User/Register/index.jsx
@@ -1,11 +1,13 @@
-import React , {useState} from 'react';
-import {Box, Button, Grid, TextField,makeStyles,InputAdornment,IconButton } from '@material-ui/core';
+import React , {useEffect, useState} from 'react';
+import {Box, Button, Grid,Select,MenuItem, TextField,makeStyles,InputAdornment,IconButton } from '@material-ui/core';
 import {VisibilityOff,Visibility} from '@material-ui/icons';
-import {register} from '../../../api'
+import {register,getAllFields,getAllOrientations} from '../../../api'
 import { Link,useHistory } from 'react-router-dom';
 import {errorsMessages} from '../../../assets/errorsMessages';
 import {snackbarTypes} from '../../../assets/snackbarTypes';
 import Snackbar from '../../../components/Snackbar';
+import DatePicker from 'react-modern-calendar-datepicker';
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
 
 const useStyles = makeStyles({
     main:{
@@ -69,16 +71,32 @@ const useStyles = makeStyles({
         width:'1.5rem',
         height:'1.5rem',
         borderRadius:'50%'
+    },
+    select:{
+        '& svg':{
+            fontSize:"1.8rem",
+            top:"20%",
+            right:"100% !important",
+            transform:'translateX(100%)'
+        },
+    },
+    cal:{
+        lineHeight: 0,
+        border: 'none',
+        borderBottom: '1px solid #f2f2f2 !important',
     }
 })
 
 export default function Register(){
     const classes = useStyles();
     const history = useHistory();
-
     const [step,setStep]= useState(false)
-
     const [showPassword,setShowPassword]= useState(false)
+    const [fields,setFields] = useState([]);
+    const [orientations,setOrientations] = useState([]);
+    const [snackOpen,setSnackOpen]=useState(false);
+    const [msg,setMsg]=useState('');
+    const [type,setType]=useState('');
 
     const [body,setBody]= useState({
         name:'',
@@ -92,47 +110,77 @@ export default function Register(){
         field:'',
     })
 
-    const [snack,setSnack]= useState({
-        status:false,
-        message:'',
-        type:''
-    })
-
-    const handleOpenSnack= (message,type)=>setSnack({
-        status:true,
-        message,
-        type
-    })
-
-    const handleCloseSnack = ()=>setSnack({
-        status:false,
-        message:'',
-        type:''
-    })
+    const handleSnackOpen = (mesg,type)=>{
+        setMsg(mesg);
+        setType(type)
+        setSnackOpen(true);
+    }
+  
+    const handleSnackClose = ()=>setSnackOpen(false)
 
     const handleClickShowPassword = ()=>setShowPassword(!showPassword)
 
     const nextPage = ()=>setStep(true);
 
     const handleRegister = ()=>{
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-        if (body.email===''||body.name===''||body.password==='') {
-            handleOpenSnack(errorsMessages.emptyField,snackbarTypes.error);
-        }else if(body.name.length<3){
-            handleOpenSnack(errorsMessages.shortName,snackbarTypes.error);
-        }else if(body.password.length<5){
-            handleOpenSnack(errorsMessages.shortPass,snackbarTypes.error);
-        }else if(!pattern.test(body.email)) {
-            handleOpenSnack(errorsMessages.invalidEmail,snackbarTypes.error);
-        }else{
-            register(body)
-            .then(()=>history.push('/verification',{phoneNumber:body.phoneNumber}))
-            .catch(({response:{data:{error}}})=>handleOpenSnack(error,snackbarTypes.error))
+        setMsg('');
+        if(
+            body.name===""||
+            body.lastName===""||
+            body.uniCode===""||
+            body.password===""||
+            body.phoneNumber===""||
+            body.email===""||
+            body.field===""||
+            body.orientation===""||
+            body.birthDate===""
+        ){
+            handleSnackOpen(errorsMessages.emptyField,snackbarTypes.error);
+        }else{ 
+            let err=false;
+            if(body.name.length<3||body.lastName.length<3){
+                err=true;
+                handleSnackOpen(old=>(`${old}:${errorsMessages.shortName}`),snackbarTypes.error);
+            }
+
+            if(body.name.password<5){
+                err=true;
+                handleSnackOpen(old=>(`${old}:${errorsMessages.shortPass}`),snackbarTypes.error);
+            }
+
+            if(body.uniCode.length!==8){ 
+                err=true;
+                handleSnackOpen(old=>(`${old}:${errorsMessages.invalidUniCode}`),snackbarTypes.error);
+            }
+            
+            const phonePattern = new RegExp(/09[0-9]{9}/)
+            const perPhonePattern = new RegExp(/۰۹[۰-۹]{9}/)
+            if(!(phonePattern.test(body.phoneNumber)||perPhonePattern.test(body.phoneNumber))){
+                err=true;
+                handleSnackOpen(old=>(`${old}:${errorsMessages.invalidPhone}`),snackbarTypes.error);
+            }
+
+            var emailPattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            if(!emailPattern.test(body.email)){ 
+                err=true;
+                handleSnackOpen(old=>(`${old}:${errorsMessages.invalidEmail}`),snackbarTypes.error);
+            }
+
+            if(!err){
+                register(body)
+                .then(()=>history.push('/verification',{phoneNumber:body.phoneNumber}))
+                .catch(({response:{data:{error}}})=>handleSnackOpen(error.join(':'),snackbarTypes.error))
+            }
         }
     }
 
+    useEffect(()=>{
+        getAllFields()
+        .then(setFields)
+    },[])
+
     return <Box className={classes.main}>
-        <Snackbar message={snack.message} type={snack.type} handleClose={handleCloseSnack} open={snack.status}/>
+        <Snackbar message={msg} type={type} handleClose={handleSnackClose} open={snackOpen}/>
         <Box className={classes.registerBox}>
             <Box className={classes.form}>
                 <h1>ثبت نام</h1>
@@ -142,6 +190,7 @@ export default function Register(){
                             <div className={classes.smallInputDiv}>
                                 <lable>نام</lable>
                                 <TextField className={classes.smallInputs} style={{width:'95%'}} label="نام" required
+                                    value={body.name}
                                     variant="outlined" 
                                     onChange={({target:{value}})=>setBody(old=>{return{...old,name:value}})}
                                 />
@@ -149,6 +198,7 @@ export default function Register(){
                             <div className={classes.smallInputDiv}>
                                 <lable>نام خانوادگی</lable>
                                 <TextField className={classes.smallInputs} label="نام خانوادگی" required
+                                    value={body.lastName}
                                     variant="outlined" 
                                     onChange={({target:{value}})=>setBody(old=>{return{...old,lastName:value}})}
                                 />
@@ -157,20 +207,36 @@ export default function Register(){
                         <div className={classes.inputDiv}>
                             <lable>ایمیل</lable>
                             <TextField className={classes.inputs} label="ایمیل" type='email' required
+                                value={body.email}
                                 variant="outlined" 
                                 onChange={({target:{value}})=>setBody(old=>{return{...old,email:value}})}
                             />
                         </div>
                         <div className={classes.inputDiv}>
                             <lable>شماره همراه</lable>
-                            <TextField className={classes.inputs} label="شماره همراه" required
+                            <TextField
+                                className={classes.inputs} label="شماره همراه" required
                                 variant="outlined" 
+                                value={body.phoneNumber}
+                                inputProps={{
+                                    maxLength:11
+                                }}
+                                onKeyDown={(e)=>{
+                                    if(
+                                        !e.key.match(/^[0-9۰-۹]+$/)
+                                        &&e.key!=='Backspace'
+                                        &&e.key!=='ArrowLeft'
+                                        &&e.key!=='ArrowRight'
+                                    )
+                                        e.preventDefault()
+                                }}
                                 onChange={({target:{value}})=>setBody(old=>{return{...old,phoneNumber:value}})}
                             />
                         </div>
                         <div className={classes.inputDiv}>
                             <lable>رمزعبور</lable>
                             <TextField className={classes.inputs} label="رمزعبور" type={showPassword?'text':'password'} required 
+                                value={body.password}
                                 InputProps={{
                                     endAdornment: (
                                         <InputAdornment position="end">
@@ -187,42 +253,70 @@ export default function Register(){
                         <Button style={{backgroundColor:'rgba(87,122,255,87%)',width:'77%',marginTop:15}} onClick={nextPage}>صفحه بعد</Button>
                     </Grid>:
                     <Grid container direction='column' wrap='nowrap' alignItems='center' justify='space-around' style={{height:'70%'}}>
-                        {/* <Grid container alignItems='center' justify='space-around' wrap='nowrap' style={{width:'80%'}} >
-                            <div className={classes.smallInputDiv}>
-                                <lable>نام</lable>
-                                <TextField className={classes.smallInputs} style={{width:'95%'}} label="نام" required
-                                    variant="outlined" 
-                                    onChange={({target:{value}})=>setBody(old=>{return{...old,name:value}})}
-                                />
-                            </div>
-                            <div className={classes.smallInputDiv}>
-                                <lable>نام خانوادگی</lable>
-                                <TextField className={classes.smallInputs} label="نام خانوادگی" required
-                                    variant="outlined" 
-                                    onChange={({target:{value}})=>setBody(old=>{return{...old,lastName:value}})}
-                                />
-                            </div> 
-                        </Grid> */}
                         <div className={classes.inputDiv}>
                             <lable>شماره دانشجویی</lable>
-                            <TextField className={classes.inputs} label="شماره دانشجویی" type='email' required
+                            <TextField
+                                className={classes.inputs} label="شماره دانشجویی" required
+                                value={body.uniCode}
+                                inputProps={{
+                                    maxLength:8
+                                }}
+                                onKeyDown={(e)=>{
+                                    if(!e.key.match(/^[۰-۹0-9]+$/)&&e.key!=='Backspace')
+                                        e.preventDefault()
+                                }}
                                 variant="outlined" 
                                 onChange={({target:{value}})=>setBody(old=>{return{...old,uniCode:value}})}
                             />
                         </div>
                         <div className={classes.inputDiv}>
-                            <lable>گرایش</lable>
-                            <TextField className={classes.inputs} label="گرایش" type='email' required
+                            <DatePicker
+                                colorPrimary="#577AFF"
+                                onChange={(e) =>setBody(old=>({...old,rawBirthDate:e,birthDate:`${e.year}/${e.month}/${e.day}`}))}
+                                value={body.rawBirthDate}
+                                locale="fa"
                                 variant="outlined" 
-                                onChange={({target:{value}})=>setBody(old=>{return{...old,orientation:value}})}
+                                renderInput={({ ref })=>
+                                    <TextField
+                                        ref={ref}
+                                        className={classes.inputs}
+                                        label="تاریخ تولد" 
+                                        required
+                                        value={body.birthDate}
+                                        variant="outlined" 
+                                    />
+                                }
                             />
                         </div>
                         <div className={classes.inputDiv}>
                             <lable>رشته</lable>
-                            <TextField className={classes.inputs} label="شماره همراه" required
-                                variant="outlined" 
-                                onChange={({target:{value}})=>setBody(old=>{return{...old,field:value}})}
-                            />
+                            <Select 
+                                className={classes.select}
+                                value={body.field}
+                                onChange={({target:{value}})=>{
+                                    getAllOrientations(value)
+                                    .then(setOrientations)
+                                    setBody(old=>({...old,field:value,orientation:''}))
+                                }}
+                            >
+                                {fields.map(item=>
+                                    <MenuItem value={item.id}>{item.name}</MenuItem>
+                                )}
+                            </Select>
+                        </div>
+                        <div className={classes.inputDiv}>
+                            <lable>گرایش</lable>
+                            <Select 
+                                className={classes.select}
+                                value={body.orientation}
+                                onChange={({target:{value}})=>{
+                                    setBody(old=>({...old,orientation:value}))
+                                }}
+                            >
+                                {orientations.map(item=>
+                                    <MenuItem value={item.id}>{item.name}</MenuItem>
+                                )}
+                            </Select>
                         </div>
                         <Button style={{backgroundColor:'rgba(87,122,255,87%)',width:'77%',marginTop:15}} onClick={handleRegister}>ثبت نام</Button>
                     </Grid>

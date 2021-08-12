81ec803465c810b48a2144cc850a51360e8d1c8a
aaallliiis <alisolatni59@icloud.com>
2021-08-12 19:02:03 +0430

    debug:cards

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,55 +1,79 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
-import {useHistory} from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   main: {
-    width:'13%',
-    margin:10,
-    height:125,
-    position:"relative",
-    padding:'1.5rem',
-    borderRadius:'0.5rem',
-    cursor:'pointer',
+    width: "13%",
+    margin: 10,
+    height: 125,
+    position: "relative",
+    padding: "1.5rem",
+    borderRadius: "0.5rem",
+    cursor: "pointer",
   },
-  title:{
-    fontFamily:'vazir-regular',
-    fontSize:18,
-    fontWeight:'bold',
-    margin:2,
-    userSelect:'none'
+  title: {
+    fontFamily: "vazir-regular",
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 2,
+    userSelect: "none",
   },
-  description:{
-    fontFamily:'vazir-regular',
-    fontSize:16,
-    margin:'7px 10px 0px 0px',
-    userSelect:'none'
+  description: {
+    fontFamily: "vazir-regular",
+    fontSize: 16,
+    margin: "7px 10px 0px 0px",
+    userSelect: "none",
   },
-  type:{
-    position:"absolute",
-    fontSize:14,
-    marginBottom:0,
-    bottom:10,
-    right:14,
+  type: {
+    position: "absolute",
+    fontSize: 14,
+    marginBottom: 0,
+    bottom: 10,
+    right: 14,
   },
 });
 
-export default function MediaCard({item:{_id,title,type,description}}){
+export default function MediaCard({ item: { id, title, type, description } }) {
   const classes = useStyles();
-  const {push} = useHistory();
+  const { push } = useHistory();
 
   return (
-    <Card className={classes.main} onClick={()=>push(`/file/${_id}`)}>
-      <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
+    <Card className={classes.main} onClick={() => push(`/file/${id}`)}>
+      <Typography
+        gutterBottom
+        variant="h5"
+        component="h2"
+        className={classes.title}
+      >
         {title}
       </Typography>
-      <Typography gutterBottom variant="div" component="div" className={classes.description}>
+      <Typography
+        gutterBottom
+        variant="div"
+        component="div"
+        className={classes.description}
+      >
         {description}
       </Typography>
-      <Typography gutterBottom variant="div" component="div" className={classes.type}>
-        نوع : {type==='PDF'?'پی دی اف':type==='IMG'?'عکس':type==='VID'?'فیلم':type==='VC'?'صوت':null}
+      <Typography
+        gutterBottom
+        variant="div"
+        component="div"
+        className={classes.type}
+      >
+        نوع :{" "}
+        {type === "PDF"
+          ? "پی دی اف"
+          : type === "IMG"
+          ? "عکس"
+          : type === "VID"
+          ? "فیلم"
+          : type === "VC"
+          ? "صوت"
+          : null}
       </Typography>
     </Card>
   );
